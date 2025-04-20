1bdd763356cd2253f98dcb2323b11cae47d031ae
daniel <dsimpson@missionbit.org>
2025-04-20 04:16:49 +0000

    Apr 19, 2025, 9:16 PM

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("test");
-console.log("babyaced");
+const menuButton = document.getElementById("menu-button");
+
+menuButton.addEventListener("click", openSidenav);
+const sidenavCloseButton = document.getElementById("sidenav-close-button");
+
+sidenavCloseButton.addEventListener("click", closeSidenav);
+
+function openSidenav() {
+  const sidenav = document.getElementById("sidenav");
+  sidenav.style.transform = "translateX(200px)";
+}
+
+function closeSidenav() {
+  const sidenav = document.getElementById("sidenav");
+  sidenav.style.transform = "translateX(-200px)";
+}
