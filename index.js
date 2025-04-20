d8b8f7b28b2427fb03d042fd37669d1a15f32103
daniel <dsimpson@missionbit.org>
2025-04-20 04:26:13 +0000

    Apr 19, 2025, 9:26 PM

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const sidenavCloseButton = document.getElementById("sidenav-close-button");
 
 sidenavCloseButton.addEventListener("click", closeSidenav);
 
+const sidenavUnderlay = document.getElementById("sidenav-underlay");
+sidenavUnderlay.addEventListener("click", closeSidenav);
+
 function openSidenav() {
   const sidenav = document.getElementById("sidenav");
   sidenav.style.transform = "translateX(200px)";
+  sidenavUnderlay.style.visibility = "visible";
 }
 
 function closeSidenav() {
   const sidenav = document.getElementById("sidenav");
   sidenav.style.transform = "translateX(-200px)";
+  sidenavUnderlay.style.visibility = "hidden";
 }
